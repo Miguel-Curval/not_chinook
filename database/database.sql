19ff5e05e986c2ae3a43385246086a09fc628104
Miguel-Curval <76057613+Miguel-Curval@users.noreply.github.com>
2023-04-28 00:41:30 +0100

    Database stub

--- database/database.sql
+++ database/database.sql
@@ -1,4 +1,68 @@
-﻿
+
+/*******************************************************************************
+   Drop Tables
+********************************************************************************/
+
+DROP TABLE IF EXISTS Comment;
+DROP TABLE IF EXISTS Department;
+DROP TABLE IF EXISTS Hashtag;
+DROP TABLE IF EXISTS Ticket;
+DROP TABLE IF EXISTS TicketHashtag;
+DROP TABLE IF EXISTS User;
+
+/*******************************************************************************
+   Create Tables
+********************************************************************************/
+
+CREATE TABLE Comment (
+    id INTEGER PRIMARY KEY,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    content TEXT NOT NULL
+);
+
+CREATE TABLE Department (
+    name TEXT PRIMARY KEY
+);
+
+CREATE TABLE Hashtag (
+    name TEXT PRIMARY KEY
+);
+
+CREATE TABLE Ticket (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    status TEXT NOT NULL,
+    createdBy TEXT REFERENCES User(username) NOT NULL,
+    beingSolvedBy TEXT REFERENCES User(username)
+    departmentName TEXT REFERENCES Department(name),
+    priority INTEGER,
+);
+
+CREATE TABLE TicketHashtag (
+    name TEXT REFERENCES Hashtag(name),
+    idTicket INTEGER REFERENCES Ticket(id),
+    PRIMARY KEY (name, idTicket)
+);
+
+CREATE TABLE User (
+    username TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+   	password TEXT NOT NULL,
+    email TEXT NOT NULL,
+    role TEXT NOT NULL,
+    departmentName TEXT REFERENCES Department(Name)
+);
+
+/*******************************************************************************
+   Create Foreign Keys
+********************************************************************************/
+
+
+/*******************************************************************************
+   Populate Tables
+********************************************************************************/
+
+
+/*﻿
 /*******************************************************************************
    Chinook Database - Version 1.4
    Script: Chinook_Sqlite.sql
@@ -4271,3 +4335,4 @@ INSERT INTO Customer (CustomerId, FirstName, LastName, Address, City, Country, P
 INSERT INTO Customer (CustomerId, FirstName, LastName, Address, City, Country, Phone, Email, Password) VALUES (57, 'Luis', 'Rojas', 'Calle Lira, 198', 'Santiago', 'Chile', '+56 (0)2 635 4444', 'luisrojas@yahoo.cl', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220');
 INSERT INTO Customer (CustomerId, FirstName, LastName, Address, City, Country, PostalCode, Phone, Email, Password) VALUES (58, 'Manoj', 'Pareek', '12,Community Centre', 'Delhi', 'India', '110017', '+91 0124 39883988', 'manoj.pareek@rediff.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220');
 INSERT INTO Customer (CustomerId, FirstName, LastName, Address, City, Country, PostalCode, Phone, Email, Password) VALUES (59, 'Puja', 'Srivastava', '3,Raj Bhavan Road', 'Bangalore', 'India', '560001', '+91 080 22289999', 'puja_srivastava@yahoo.in', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220');
+*/
